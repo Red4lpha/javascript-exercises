9eabfad7da178ea9127be3c44f622f116a7c0b94
Kevin S <alphalop42@gmail.com>
2020-08-29 21:22:12 -0700

    Completion to reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wLength = word.length;
+    let result = '';
+    for(let i = 0; i < wLength; i++){
+        result += word.charAt(wLength-(i+1));
+    }
+    return result;
 }
 
 module.exports = reverseString
