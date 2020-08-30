ebbc50d0f61423a1bf710c9087b4bb126d1616a0
Kevin S <alphalop42@gmail.com>
2020-08-30 16:22:25 -0700

    Completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(initialArray,...removalList) {
+    let results = initialArray;
+    for(let x = 0; x < results.length; x++){
+        for(y = 0; y < removalList.length; y++){
+            if (results[x] === removalList[y]){
+                results.splice(x,1);
+                //since our array decreases in size, so must the irriator
+                x--;
+                }
+        }        
+    }
+return results;
 }
-
 module.exports = removeFromArray
+
