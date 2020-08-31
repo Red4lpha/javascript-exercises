684942c69a21573a0a6b332de817fb86c8b96a87
Kevin S <alphalop42@gmail.com>
2020-08-30 17:51:29 -0700

    Completetion of leapYear

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //this is a leap year...unless..
+    if ((year%4) == 0 ){
+        //not a leap year..unless
+        if ((year%100) == 0){
+            //a leap year
+            if ((year%400)==0) {return true;}
+            else return false;
+        }
+        return true;
+    }
+    else return false;
 }
 
 module.exports = leapYears
