2e1739b1d443f7226efefc749db14b92b72bf049
Kevin S <alphalop42@gmail.com>
2020-08-30 17:31:03 -0700

    completetion of sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    //we dont allow negative numbers
+    if ((firstNum || secondNum) < 0){return 'ERROR'; } 
+    else if ((typeof firstNum && typeof secondNum) === "number" ) {
+    
+        //sorts the higher number to first position
+        sortNumbers = function(firstNum, secondNum){
+            if (firstNum >= secondNum)  { return [firstNum, secondNum] }
+            else { return [secondNum, firstNum]; }
+        }
+        let sorted = sortNumbers(firstNum, secondNum);
+        let biggerNum = sorted[0];
+        let smallerNum = sorted[1];
+        let sum = 0;
+        //if (biggerNum == smallerNum) {return }
+        while (smallerNum <= biggerNum) {
+            sum += smallerNum;
+            smallerNum++;
+        } 
+        return sum;
+    }
+    //all else return ERROR
+    else return 'ERROR'; 
+
 
 }
 
