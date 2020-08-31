8f47f6e528b67e03b9fdcdeae86f439cbc805cf9
Kevin S <alphalop42@gmail.com>
2020-08-30 18:25:57 -0700

    Completetion of tempConversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+const ftoc = function(num) {
+  let results = ((num-32)*(5/9));
+  //rounding
+  results = round(results, 1);
+  return results;
 }
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let results = ((num*(9/5)+32));
+  //rounding
+  results = round(results, 1);
+  return results;
 }
 
 module.exports = {
