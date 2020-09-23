1292c3c9398e55f35c4e5a40d43ba9273dadc9ef
Kevin S <alphalop42@gmail.com>
2020-09-22 19:34:19 -0700

    Committing completion of calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
-function add () {
+function add (x ,y) {
+	return x + y;
 	
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (numList) {
+	let totalSum = 0;
+	for(i = 0; i < numList.length; i++) {totalSum += parseInt(numList[i], 10);}
+	return totalSum;
 }
 
-function multiply () {
-	
+function multiply (numList) {
+	let total = 1;
+	for(i = 0; i < numList.length; i++) {total *= parseInt(numList[i], 10);}
+	return total;
 }
 
-function power() {
-	
+function power(base, power ) {
+	let total = 1;
+	for(i = 0; i < power; i++) {total *= parseInt(base, 10);}
+	return total;
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if(num === 0) {return 1;}
+	else {
+		let total = 1;
+		for(i = num; i > 0; i--) {total *= parseInt(i, 10);}
+		return total;
+	}
 }
 
 module.exports = {
