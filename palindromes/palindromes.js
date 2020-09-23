f97f4ef5952da80aeca6173ccf3f6933e05e53d7
Kevin S <alphalop42@gmail.com>
2020-09-22 21:47:06 -0700

    Completion of palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
-
+const palindromes = function(rawPalin) {
+    let trimPalin = rawPalin.replace(/[ ,.!]/g, "");
+    trimPalin = trimPalin.toLowerCase();
+    //return trimPalin;
+    for(i = 0; i < trimPalin.length; i++){
+        if (trimPalin[i] != trimPalin[(trimPalin.length-1)-i]) {return false;} 
+    }
+    return true;  
 }
 
 module.exports = palindromes
