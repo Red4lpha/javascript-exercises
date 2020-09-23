0eb75a8be588f62cbcbc85e863cf52525f3452f3
Kevin S <alphalop42@gmail.com>
2020-09-23 00:32:55 -0700

    Completion of fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(place) {
+    if (place < 1){return "OOPS";}
+    let prev = 0;
+    let current = 1;
+    let temp = 0;
+    for (i = 2;i <= place; i++){
+        temp = prev + current;
+        prev = current;
+        current = temp;
+    }
+    return current;
 }
 
 module.exports = fibonacci
