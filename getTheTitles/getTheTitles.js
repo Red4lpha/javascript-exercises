f658c9d2cb372a05e456bf141bb32df3443189c6
Kevin S <alphalop42@gmail.com>
2020-09-23 23:19:44 -0700

    Completion of getTheTitles

--- getTheTitles/getTheTitles.js
+++ getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(objArray) {
+    let first = objArray[0];
+    let second = objArray[1];
+    let temp = [];
+    temp[0] = first.title; 
+    temp[1] = second.title; 
+    return temp;
 }
 
 module.exports = getTheTitles;
